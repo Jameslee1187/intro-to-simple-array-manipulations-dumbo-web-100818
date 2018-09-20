cc5649307ec05a35f8682406c60f169fa43a018f
jameslee1187 <jameslee1187@gmail.com>
2018-09-20 08:09:57 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,5 +30,8 @@ def using_flatten(private_colleges_in_newyork)
 end
 def using_delete(instructors, string)
   instructors.delete(string)
+end using_delete_at(array, integer)
+array.delete_at(integer)
 end
+
 
